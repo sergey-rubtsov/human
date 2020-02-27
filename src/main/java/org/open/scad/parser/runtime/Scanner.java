16bf0fb3e546b654c5991ca392c47efa2c84367c
Sergey Rubtsov <Sergey_Rubtsov@rcomext.com>
2020-02-27 18:42:15 +0200

    Add jay compiler, remove cup

--- src/main/java/org/open/scad/parser/runtime/Scanner.java
+++
@@ -1,6 +0,0 @@
-package org.open.scad.parser.runtime;
-
-public interface Scanner {
-    /** Return the next token, or <code>null</code> on end-of-file. */
-    public Symbol next_token() throws java.lang.Exception;
-}
